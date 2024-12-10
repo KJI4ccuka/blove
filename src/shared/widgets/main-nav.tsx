e6d59a48535e45aeb896c66b8d235583c3bf055d
David <grikurovdavid@gmail.com>
2024-12-10 20:58:01 +0100

    update

--- src/shared/widgets/main-nav.tsx
+++ src/shared/widgets/main-nav.tsx
@@ -1,11 +1,10 @@
 'use client'
 
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
 import { Tabbar } from '@telegram-apps/telegram-ui'
-import { Icon24QR } from '@telegram-apps/telegram-ui/dist/icons/24/qr'
-import { Icon28Chat } from '@telegram-apps/telegram-ui/dist/icons/28/chat'
-import { Icon28Heart } from '@telegram-apps/telegram-ui/dist/cjs/icons/28/heart'
-import { Link } from '@/shared/components/ui/link/link'
+import { Icon24Globe, Icon24Heart, Icon24Person } from '@/shared/icons/icons'
 
 interface IProps {
   className?: string
@@ -16,41 +15,43 @@ const tabs = [
     id: 'Liked',
     text: 'Liked',
     href: '',
-    Icon: Icon28Heart
+    Icon: Icon24Heart
   },
   {
     id: 'Home',
     text: 'Home',
     href: '/home',
-    Icon: Icon24QR
+    Icon: Icon24Globe
   },
   {
     id: 'Profile',
     text: 'Profile',
     href: '/profile',
-    Icon: Icon28Chat
+    Icon: Icon24Person
   }
 ]
 
 export const Navigation: React.FC<IProps> = ({ className }) => {
   const [currentTab, setCurrentTab] = useState(tabs[1].id)
+  const router = useRouter()
+
+  const handleTabClick = (id: string, href: string) => {
+    setCurrentTab(id)
+    router.push(href)
+  }
 
   return (
     <Tabbar>
-      {tabs.map((
-          {
-            id,
-            text,
-            href,
-            Icon
-          }) => (
-          <Link key={id} href={href}>
-            <Tabbar.Item text={text} selected={id === currentTab} onClick={() => setCurrentTab(id)}>
-              <Icon />
-            </Tabbar.Item>
-          </Link>
-        )
-      )}
+      {tabs.map(({ id, text, href, Icon }) => (
+        <Tabbar.Item
+          key={id}
+          text={text}
+          selected={id === currentTab}
+          onClick={() => handleTabClick(id, href)}
+        >
+          <Icon />
+        </Tabbar.Item>
+      ))}
     </Tabbar>
   )
 }
