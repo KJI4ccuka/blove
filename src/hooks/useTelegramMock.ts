8f4486324c396b94b71056c184461b32b14e22b8
David <grikurovdavid@gmail.com>
2024-12-12 21:05:39 +0100

    update

--- src/hooks/useTelegramMock.ts
+++ src/hooks/useTelegramMock.ts
@@ -1,11 +1,11 @@
-import { useClientOnce } from '@/hooks/useClientOnce';
+import { useClientOnce } from '@/hooks/useClientOnce'
 import {
   isTMA,
   type LaunchParams,
   mockTelegramEnv,
   parseInitData,
-  retrieveLaunchParams,
-} from '@telegram-apps/sdk-react';
+  retrieveLaunchParams
+} from '@telegram-apps/sdk-react'
 
 /**
  * Mocks Telegram environment in development mode.
@@ -13,15 +13,15 @@ import {
 export function useTelegramMock(): void {
   useClientOnce(() => {
     if (!sessionStorage.getItem('env-mocked') && isTMA('simple')) {
-      return;
+      return
     }
 
     // Determine which launch params should be applied. We could already
     // apply them previously, or they may be specified on purpose using the
     // default launch parameters transmission method.
-    let lp: LaunchParams | undefined;
+    let lp: LaunchParams | undefined
     try {
-      lp = retrieveLaunchParams();
+      lp = retrieveLaunchParams()
     } catch (e) {
       const initDataRaw = new URLSearchParams([
         ['user', JSON.stringify({
@@ -31,14 +31,14 @@ export function useTelegramMock(): void {
           username: 'rogue',
           language_code: 'en',
           is_premium: true,
-          allows_write_to_pm: true,
+          allows_write_to_pm: true
         })],
         ['hash', '89d6079ad6762351f38c6dbbc41bb53048019256a9443988af7a48bcad16ba31'],
         ['auth_date', '1716922846'],
         ['start_param', 'debug'],
         ['chat_type', 'sender'],
-        ['chat_instance', '8428209589180549439'],
-      ]).toString();
+        ['chat_instance', '8428209589180549439']
+      ]).toString()
 
       lp = {
         themeParams: {
@@ -54,19 +54,19 @@ export function useTelegramMock(): void {
           sectionBgColor: '#17212b',
           sectionHeaderTextColor: '#6ab3f3',
           subtitleTextColor: '#708499',
-          textColor: '#f5f5f5',
+          textColor: '#f5f5f5'
         },
         initData: parseInitData(initDataRaw),
         initDataRaw,
         version: '8',
-        platform: 'tdesktop',
+        platform: 'tdesktop'
       }
     }
 
-    sessionStorage.setItem('env-mocked', '1');
-    mockTelegramEnv(lp);
+    sessionStorage.setItem('env-mocked', '1')
+    mockTelegramEnv(lp)
     console.warn(
-      '⚠️ As long as the current environment was not considered as the Telegram-based one, it was mocked. Take a note, that you should not do it in production and current behavior is only specific to the development process. Environment mocking is also applied only in development mode. So, after building the application, you will not see this behavior and related warning, leading to crashing the application outside Telegram.',
-    );
-  });
+      '⚠️ As long as the current environment was not considered as the Telegram-based one, it was mocked. Take a note, that you should not do it in production and current behavior is only specific to the development process. Environment mocking is also applied only in development mode. So, after building the application, you will not see this behavior and related warning, leading to crashing the application outside Telegram.'
+    )
+  })
 }
